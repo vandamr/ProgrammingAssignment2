420d8686af73fbb172de4477a6feadb3469d0c10
vandamr <vandamr@hotmail.com>
2015-08-12 17:02:37 +0200

    updated cachematrix.R with my code
    cachematrix.R contains the code for R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Data Scientist - R Programming W3 - assignment 2
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a list of 4 matrix functions for matrix x
+makeCacheMatrix <- function(x=matrix()) {
+  inv <- NULL
+  setmatrix <- function(y) {                   ## $setmatrix Sets the data matrix in cache
+    x <<- y
+    inv <<- NULL
+  }
+  getmatrix <- function() x                    ## $getmatrix gets the data matrix from cache
+  setinverse <- function(solve) inv <<- solve  ## $setinverse sets the inverse matrix in cache
+  getinverse <- function() inv                 ## $getinverse gets the inverse matrix from cache
+  list(setmatrix=setmatrix,                    ## return the list of 4 functions
+       getmatrix=getmatrix,
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Calculates the inverse of the matrix x. If the inverse
+## is already calculated, it will not calculate but take it from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()                        ## get the inverse matrix from cache
+  if(!is.null(inv)) {                          ## determine if the inverse is already calculated
+    message("getting inverse from cache")      ## print the message to the console
+    return(inv)                                ## return the inverse matrix
+  }
+  matrix <- x$getmatrix()                      ## get the data matrix from cache
+  inv <- solve(matrix)                         ## calculate the inverse matrix
+  x$setinverse(inv)                            ## set the inverse matrix in cache
+  inv                                          ## return the inverse matrix
 }
